31e7b429a6a00ded1762932a33d9db1fc79f689f
AIMEE505 <cociocaimee505@yahoo.com>
2024-01-31 17:13:29 +0200

    speram

--- js/modal.js
+++ js/modal.js
@@ -12,3 +12,22 @@
       refs.modal.classList.toggle("is-hidden");
     }
   })();
+
+  (() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-menu-open]"),
+      closeModalBtn: document.querySelector("[data-menu-close]"),
+      modal: document.querySelector("[data-menu]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", openModal);
+    refs.closeModalBtn.addEventListener("click", closeModal);
+  
+    function openModal() {
+      refs.modal.style.visibility = "visible";
+    }
+  
+    function closeModal() {
+      refs.modal.style.visibility = "hidden";
+    }
+  })();
